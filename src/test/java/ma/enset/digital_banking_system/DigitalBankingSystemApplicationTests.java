6ede76990ce0c205c84bea729392625638b82514
Abdex <abdex@gmail.com>
2025-04-28 17:14:03 +0100

    init

---
+++ src/test/java/ma/enset/digital_banking_system/DigitalBankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.digital_banking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalBankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
